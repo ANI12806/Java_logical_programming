efa7170209250cf5574fbaae4603290510738e02
ANI12806 <anithasanam128@gmail.com>
2024-11-07 16:40:33 +0530

    Add files via upload

---
+++ DAY-1/Anitha.java
@@ -0,0 +1,14 @@
+//Sample program
+/*
+sample
+java
+program
+*/
+import java.lang.*;
+class Anitha
+{
+public static void main(String args[])
+{
+System.out.print("THIS IS MY FIRST PROGRAM");
+}
+}
---
+++ DAY-1/Employee.java
@@ -0,0 +1,12 @@
+class Employee
+{
+public static void main(String args[])
+{
+int eno=101;
+String ename="Anitha";
+float esal=544.54F;
+System.out.println("The Given employee Number is:"+eno);
+System.out.println("The Given employee Name is:"+ename);
+System.out.println("The Given employee Salary is:"+esal);
+}
+}
